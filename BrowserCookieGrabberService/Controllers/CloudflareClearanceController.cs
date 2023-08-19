ff0666cf4583b6f8288dc09dd7d9fb0d1798c963
milleniumbug <milleniumbug@protonmail.com>
2023-08-19 17:10:43 +0200

    Add more logging to the cookie grabber service

--- BrowserCookieGrabberService/Controllers/CloudflareClearanceController.cs
+++ BrowserCookieGrabberService/Controllers/CloudflareClearanceController.cs
@@ -1,4 +1,5 @@
 using Common.Models;
+using FanficScraper.Utils;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Net.Http.Headers;
 
@@ -7,13 +8,21 @@ namespace BrowserCookieGrabberService.Controllers;
 [ApiController]
 public class CloudflareClearanceController : ControllerBase
 {
+    private readonly ILogger<CloudflareClearanceController> logger;
+
+    public CloudflareClearanceController(ILogger<CloudflareClearanceController> logger)
+    {
+        this.logger = logger;
+    }
+
     [HttpGet("ClearForWebsite")]
     public async Task<CloudflareClearanceResult> ClearForWebsite(
         [FromQuery] string url,
         [FromServices] FirefoxCookieGrabber cookieGrabber,
         [FromServices] UserAgentGrabber userAgentGrabber)
     {
-        var cookies = await cookieGrabber.GrabCookiesForSite(url);
+        var cookies = (await cookieGrabber.GrabCookiesForSite(url)).ToList();
+        logger.LogInformation("Grabbed cookies for site {0}. Cookie names: {1}", url, new ToStringableReadOnlyList<string>(cookies.Select(cookie => cookie.Name).ToList()));
         return new CloudflareClearanceResult()
         {
             UserAgent = userAgentGrabber.UserAgent,
@@ -25,8 +34,9 @@ public class CloudflareClearanceController : ControllerBase
     public string SetUserAgent(
         [FromServices] UserAgentGrabber userAgentGrabber)
     {
-        userAgentGrabber.SetUserAgent(HttpContext.Request.Headers[HeaderNames.UserAgent]);
-
+        var ua = HttpContext.Request.Headers[HeaderNames.UserAgent];
+        userAgentGrabber.SetUserAgent(ua);
+        logger.LogInformation("User agent set to {0}.", ua);
         return "Success: You can close the tab.";
     }
 }
